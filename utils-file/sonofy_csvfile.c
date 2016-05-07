38ac053d5c7c19ceecb9c25dbf56f1e74c03bf1a
Alessio Degani <alessio.degani@gmail.com>
2016-05-07 17:22:45 +0200

    Add: utils_csvfile placeholder

---
+++ utils-file/sonofy_csvfile.c
@@ -0,0 +1,11 @@
+/*
+ * libsonofy - utils-file
+ * sonofy_csvfile.c
+ *
+ * Read and write Comma Separated Values files
+ * 2016, Alessio Degani <alessio.degani@gmail.com>
+ *
+ */
+
+#include "sonofy_csvfile.h"
+
---
+++ utils-file/sonofy_csvfile.h
@@ -0,0 +1,13 @@
+/*
+ * libsonofy - utils-file
+ * sonofy_csvfile.h
+ *
+ * Read and write Comma Separated Values files
+ * 2016, Alessio Degani <alessio.degani@gmail.com>
+ *
+ */
+
+#ifndef LIBSONOFY_CSVFILE_H
+#define LIBSONOFY_CSVFILE_H
+
+#endif // LIBSONOFY_CSVFILE_H
