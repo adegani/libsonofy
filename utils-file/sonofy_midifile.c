73f9bb4f9d2aea716cc2ecde48674433aed96612
Alessio Degani <alessio.degani@gmail.com>
2016-05-07 16:51:31 +0200

    Add: utils-midifile placeholder

---
+++ utils-file/sonofy_midifile.c
@@ -0,0 +1,11 @@
+/*
+ * libsonofy - utils-file
+ * sonofy_midifile.c
+ *
+ * Read and write MIDI files
+ * 2016, Alessio Degani <alessio.degani@gmail.com>
+ *
+ */
+
+#include "sonofy_midifile.h"
+
---
+++ utils-file/sonofy_midifile.h
@@ -0,0 +1,13 @@
+/*
+ * libsonofy - utils-file
+ * sonofy_midifile.h
+ *
+ * Read and write MIDI files
+ * 2016, Alessio Degani <alessio.degani@gmail.com>
+ *
+ */
+
+#ifndef LIBSONOFY_MIDIFILE_H
+#define LIBSONOFY_MIDIFILE_H
+
+#endif // LIBSONOFY_MIDIFILE_H
