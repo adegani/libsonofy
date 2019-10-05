beaa8d9d0158e8f9ace79cccd2aaba2dc9a8940f
Alessio Degani <alessio.degani@gmail.com>
2019-10-05 10:01:00 +0200

    Update ex_wavefile.c

--- examples/ex_wavefile.c
+++ examples/ex_wavefile.c
@@ -6,12 +6,11 @@ void main(){
 
 	int16_t *samples = NULL;
 
-    wERR err = sonofy_wavread("440.wav", &samples, &header);
+    	wERR err = sonofy_wavread("440.wav", &samples, &header);
 
-    if ( err != 0 ){
+    	if ( err != 0 ){
 		printf("err: %d\n", err);
 	}else{
-
 		printf("Channel count: \t%d \n",    header.numChannels);
 		printf("Sample rate: \t%d Hz\n", 	header.sampleRate);
 		printf("Samples format: %d bit\n", header.bitsPerSample);
